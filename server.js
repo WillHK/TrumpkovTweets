49fd559479327276daf846d5d65ad42c05d07cbf
Will <W@Wills-MacBook-Pro.local>
2017-11-13 12:00:40 -0800

    new markov package

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 const fs = require('fs');
-const markov = require('markov');
-require('dotenv').config();
+// const markov = require('markov');
+// require('dotenv').config();
 
-let m = markov(1);
-let s = fs.createReadStream(__dirname + '/tweets.txt');
-m.seed(s, function() {
-  var stdin = process.openStdin();
-  console.log('> ');
+// let m = markov(1);
+// let s = fs.createReadStream(__dirname + '/tweets.txt');
+// m.seed(s, function() {
+//   var stdin = process.openStdin();
+//   console.log('> ');
   
-  stdin.on('data', function (line) {
-      var res = m.respond(line.toString()).join(' ');
-      console.log(res);
-      console.log('> ');
-  });
-});
+//   stdin.on('data', function (line) {
+//       var res = m.respond(line.toString()).join(' ');
+//       console.log(res);
+//       console.log('> ');
+//   });
+// });
 
+const MarkovChain = require('markovchain');
+let quotes = new MarkovChain(fs.readFileSync('./tweets.txt', 'utf8'));
 
+console.log(quotes.start('The').end(10).process());
 
